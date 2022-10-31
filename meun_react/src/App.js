3e7ffbf5c7553247ecddc35c55d26cb42f261b73
Serigne Saliou Mbacké NDIAYE <serignesaliou993@gmail.com>
2022-10-31 00:28:00 +0000

    c

--- meun_react/src/App.js
+++ meun_react/src/App.js
@@ -6,19 +6,23 @@ import {BrowserRouter as Router, Route  } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar/>
-        <div>
-          <Route path='/'>
-            <Home/>
-          </Route>
-          <Route path='/create'>
-            <Create/>
-          </Route>
-        </div>
-      </div>
-    </Router>
+    // <Router>
+    //   <div>
+    //     <Navbar/>
+    //     <div className='content'>
+    //       <Route path='/'>
+    //         <Home/>
+    //       </Route>
+    //       <Route path='/create'>
+    //         <Create/>
+    //       </Route>
+    //     </div>
+    //   </div>
+    // </Router>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
   );
 }
 
