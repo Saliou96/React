66a46db2ef7a21c41a83528222d769b9271a7955
Serigne Saliou Mbacké NDIAYE <serignesaliou993@gmail.com>
2022-11-11 12:41:21 +0000

    Update App.js

--- meun_react/src/App.js
+++ meun_react/src/App.js
@@ -1,29 +1,21 @@
-import './App.css';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import Create from './components/Create';
-import {BrowserRouter as Router, Route  } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    // <Router>
-    //   <div>
-    //     <Navbar/>
-    //     <div className='content'>
-    //       <Route path='/'>
-    //         <Home/>
-    //       </Route>
-    //       <Route path='/create'>
-    //         <Create/>
-    //       </Route>
-    //     </div>
-    //   </div>
-    // </Router>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  const [count, setCount] = useState(0);
 
+  // Équivalent à componentDidMount plus componentDidUpdate :
+  useEffect(() => {
+    // Mettre à jour le titre du document en utilisant l'API du navigateur
+    document.title = `Vous avez cliqué ${count} fois`;
+  });
+
+  return (
+    <div>
+      <p>Vous avez cliqué {count} fois</p>
+      <button onClick={() => setCount(count + 1)}>
+        Cliquez ici
+      </button>
+    </div>
   );
 }
-
 export default App;
