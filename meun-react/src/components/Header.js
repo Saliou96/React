c636c4c2a98658eab6626f84e52a50e0f0ed67b9
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2022-11-15 12:49:31 +0000

    Update Header.js

--- meun-react/src/components/Header.js
+++ meun-react/src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { AppBar,Button,Stack,Toolbar, Typography } from '@mui/material';
+import { AppBar,Toolbar, Typography } from '@mui/material';
 
 
 function Header() {
   return (
-      <AppBar position='static'>
+      <AppBar>
         <Toolbar>
           <Typography>Les pains du lendemain</Typography>
-          <Stack spacing={2} direction='row' sx={{marginLeft:'auto'}}>
+          {/* <Stack spacing={2} direction='row' sx={{marginLeft:'auto'}}>
             <Button variant='text' color='inherit' href='A_propos'>A propos</Button>
             <Button variant='text' color='inherit' href='Category'>Category</Button>
             <Button variant='text' color='inherit' href='Card'>Card</Button>
             <Button variant='text' color='inherit' href='Contact'>Contact</Button>
-          </Stack>
+          </Stack> */}
         </Toolbar>
       </AppBar>
   )
