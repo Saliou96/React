e168251f96ff7d2e4664d00946361a52ae148659
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2022-11-16 01:58:10 +0000

    aa

--- meun-react/src/App.js
+++ meun-react/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 // import Footer from './components/Footer';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-
+      <Navbar/>
       {/* <Footer/> */}
     </div>
   );
--- meun-react/src/components/Footer.js
+++ meun-react/src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './all.css'
 
 function Footer() {
   return (
