a6cf12f24b1835d74f9345830736c9cae02c7d73
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2022-11-18 13:40:38 +0000

    a

--- step/src/components/Home.js
+++ step/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Carte from './Carte'
 
 function Home() {
 
   return (
     <div className='mt-5 d-flex justify-content-center'>
-        <h1>This is Home page!!!!</h1>
+        <h1>This is Home page!!!!</h1><br/>
+        <Carte/>
     </div>
   )
 }
