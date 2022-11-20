5264da5d34e0510a9fd88d27ea9fa771c4713159
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2022-11-20 18:17:39 +0000

    f

--- step/src/App.js
+++ step/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
-// import Header from './components/Header';
+import Header from './components/Header';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Navigation from './components/Navigation';
@@ -8,9 +8,10 @@ import Navigation from './components/Navigation';
 function App() {
   return (
     <div className="App">
-      {/* <Header sant='Saliou' att={21}/> */}
       <Navigation/>
       <h3 className='m-3 d-flex justify-content-center'>MEUN REACT PAR FORCE</h3>
+      <Header sant='Saliou' att={21} niv='MGLSI'/>
+
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>} />
--- step/src/components/Header.js
+++ step/src/components/Header.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 
-function Header({sant,att}) {
+function Header({sant,att,niv}) {
 
     const [name,setName]=useState(sant);
     const [age,setAge]=useState(att);
+    const [classe,setClasse]=useState(niv);
     
     const update = () => {
         setName('lamoth');
         setAge('22');
+        setClasse('DIC2');
     }
 
     const reset = () => {
         setName('saliou');
         setAge('21');
+        setClasse('MGLSI');
     }
-
 
   return (
     <div>
-      <h1>My name is {name} and i have {age} years old.</h1>
+      <h1>My name is {name} and i have {age} years old. I am in {classe} at Polytechnic School of Dakar </h1>
       <Button variant="success" onClick={update}>Update</Button>
       <Button variant="warning" onClick={reset}>Reset</Button>
     </div>
