d3cd0541530cb95969c5147b3b1335a4b851554e
Serigne Saliou Mbacké NDIAYE <68548037+Saliou96@users.noreply.github.com>
2023-01-03 01:10:36 +0000

    a

---
+++ quora/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import './App.css';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
